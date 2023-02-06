01a663dfd4d8114239027086e931d8e222d506ff
Lohan batista <lohan.batista@digitalbusiness.com,br>
2023-02-06 11:38:15 -0300

    teste de primeiro commit

--- ToDoApp/src/screens/Exemplo1/index.tsx
+++ ToDoApp/src/screens/Exemplo1/index.tsx
@@ -1,1 +1,13 @@
 //SCREEN
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
